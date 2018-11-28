73194e8bebdd85afa26a71d7f929bc8565ad3b73
DESKTOP-URHLUII\chris ellis <c2_trenholm@hotmail.com>
2018-11-28 13:12:53 -0400

    commit

---
+++ unity3D_project_2/Assets/Scripts/CoinController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour {
+	public float RotationSpeed = 100;
+	
+	
+	// Update is called once per frame
+	void Update () {
+		float angleRot = RotationSpeed * Time.deltaTime;
+		transform.Rotate(Vector3.up * angleRot, Space.World);
+	}
+}
---
+++ unity3D_project_2/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+public float range = 2;
+public float speed = 3;
+int direction = 1;
+private Vector3 initialPos;
+	// Use this for initialization
+	void Start () {
+		initialPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float movementZ = speed * Time.deltaTime * direction;
+		float newZ = transform.position.z + movementZ;
+		if (Mathf.Abs(newZ - initialPos.z) > range){
+			direction *= -1;
+		}
+		else{
+			transform.position += new Vector3(0,0,movementZ);
+		}
+	}
+}
